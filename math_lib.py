6a109080ebf2e622677c12c6d0746e0c01c31f7c
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-12-08 00:47:14 +0500

    Yangi fayl qo'shildi

--- math_lib.py
+++ math_lib.py
@@ -3,10 +3,11 @@ import math
 pi = 3.14
 
 son = 20
-a, b ,d = 1, 2, 500
-print(round(pi)) # Sonning butun qismini olib beradi
+a , b ,d = 10 , 20, 30
+
+print(math.floor(pi)) # Sonni butun qismini olib beradi
+print(round(pi)) # Sonni butun qismini olib beradi
 print(math.ceil(pi)) #Sondan bitta katta sonni olib beradi
-print(math.floor(pi)) # Sonning butun qismini olib beradi
-print(round(math.sqrt(son))) # Sonning ildizni topib beradi
-print(max(a, b, d)) # Sonlarning maksimal qiymatini olib beradi
-print(pow(son, 100)) # Sonni daraja ko'tarib beradi
+print(round(math.sqrt(son))) #Sonni ildizini hisoblab beradi
+print(pow(son, 2)) # Sonni daraja ko'tarib beradi
+print(max(a, b ,d)) # Sonlarning eng katta olib beradi
