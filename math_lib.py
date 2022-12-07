701fab2c9321d36a3743c4c983dacce7ce401c6c
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-12-08 00:24:59 +0500

    Yangi fayl

---
+++ math_lib.py
@@ -0,0 +1,12 @@
+import math
+
+pi = 3.14
+
+son = 20
+a, b ,d = 1, 2, 500
+print(round(pi)) # Sonning butun qismini olib beradi
+print(math.ceil(pi)) #Sondan bitta katta sonni olib beradi
+print(math.floor(pi)) # Sonning butun qismini olib beradi
+print(round(math.sqrt(son))) # Sonning ildizni topib beradi
+print(max(a, b, d)) # Sonlarning maksimal qiymatini olib beradi
+print(pow(son, 100)) # Sonni daraja ko'tarib beradi
